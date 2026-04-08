6246c5a61d54d81af9b1fdce3973bb4896e3d0b1
VitorColley <117863451+VitorColley@users.noreply.github.com>
2026-04-08 10:14:28 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function APP(){
+  return(
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <div className="rounded-exl bg-white p-10 shadow-md">
+        <h1 className="text-3x1 font-bold text-s;ate-900">Lesson Planner Frontend</h1>
+        <p className="mt-3 text-slate-600">React + Vite + Tailwind is working!</p>
+      </div>
+    </div>
+  );
+}
+
+export default APP;
